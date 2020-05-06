bcf258fe6ad856d08d2d543f6480f088f7d48b78
Sandeep Prasad <s3g007@gmail.com>
2020-05-06 23:26:59 +0530

    Added minified bundle output

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,30 @@
 import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
 
-const outDir = 'dist';
+const outFile = 'dist/@mewui/core/umd/mewui';
 
 export default {
   input: 'src/index.ts',
   plugins: [typescript()],
   output: [
     {
+      file: `${outFile}.js`,
       name: 'MewUI',
-      dir: `${outDir}/@mewui/core/umd`,
       format: 'umd',
       sourcemap: true,
+    },
+    {
+      file: `${outFile}.min.js`,
+      name: 'MewUI',
+      format: 'umd',
+      sourcemap: true,
+      plugins: [
+        terser({
+          output: {
+            comments: false,
+          },
+        }),
+      ],
     },
   ],
 };
